e5afc958cb4921b506b6cf241fd77c70f7284828
eddie9110 <edwinmaregwa@gmail.com>
2023-08-10 06:42:52 -0700

    print statement changed

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letr in range(97, 123):
-    if chr(letr) is not 'e' or chr(letr) is not 'q':
-        print(f"{chr(letr)}", end="")
+    if letr != 101 or letr != 113:
+        print("{}".format(chr(letr)), end="")
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-# 4-print_hexa.py
-
-"""Program to print numbers 0 to 98 in decimal and hexadecimal."""
+# Program to print numbers 0 to 98 in decimal and hexadecimal
 
 for numb in range(0, 99):
-    print(f"{numb} = {hex(numb)}")
+    print("{} = {}".format(numb, hex(numb)))
