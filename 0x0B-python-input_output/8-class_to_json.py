05526e6598e9ffcfcc333ba37fc4e0f717fd6fbe
eddie9110 <edwinmaregwa@gmail.com>
2023-09-12 18:53:39 -0700

    pycodestyle compliant

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -6,9 +6,10 @@ A module for the function class_to_json()
 
 def class_to_json(obj):
     """
-    A function that returns the turns the 
+    A function that returns the turns the
     dictionary representation of a simple data structure
     for JSON serialization of an object.
+
     Args:
         obj: An object
 
